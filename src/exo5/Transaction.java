5d8e65c67c01b3d85759440ffbc8e29b56ac7247
colombefioren <colomberakotonjanahary@gmail.com>
2025-05-14 13:45:31 +0300

    add: fifth exo

---
+++ src/exo5/Transaction.java
@@ -0,0 +1,20 @@
+package exo5;
+
+import java.util.Date;
+
+public class Transaction {
+  private char type;
+  private double amount;
+
+  private double balance;
+  private Date date;
+  private String description;
+
+  Transaction(char type, double amount, double balance, String description) {
+    this.type = type;
+    this.amount = amount;
+    this.balance = balance;
+    this.description = description;
+    this.date = new Date();
+  }
+}
