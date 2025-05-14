aa866e315eda36be55b7d69fa9609bd8338cfcb7
colombefioren <colomberakotonjanahary@gmail.com>
2025-05-14 11:32:06 +0300

    add: additional exo1 instructions

--- src/exo1/Main.java
+++ src/exo1/Main.java
@@ -1,9 +1,29 @@
 package exo1;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    Rectangle myRectangle = new Rectangle(12, 40);
-    myRectangle.setHeight(45);
-    System.out.println(myRectangle.getPerimeter());
+    Rectangle[] rectangleArray = new Rectangle[3];
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("You are going to create 3 rectangles!");
+    for (int i = 0; i < 3; i++) {
+      System.out.println("Rectangle " + (i + 1) + " :");
+      System.out.print("Width : ");
+      double tempWidth = scanner.nextDouble();
+      System.out.print("Height : ");
+      double tempHeight = scanner.nextDouble();
+      rectangleArray[i] = new Rectangle(tempWidth, tempHeight);
+    }
+    printRectangles(rectangleArray);
+  }
+
+  public static void printRectangles(Rectangle[] array) {
+    for (int i = 0; i < array.length; i++) {
+      System.out.println();
+      System.out.println("Rectangle " + (i + 1) + " :");
+      System.out.println("Width : " + array[i].getWidth());
+      System.out.println("Height : " + array[i].getHeight());
+    }
   }
 }
